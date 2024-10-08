266b1d486588f341d2df9a1fdcd4190535ff06d4
Arisforprogramming <codegarden23@gmail.com>
2024-10-07 20:33:55 -0500

    Changed new branch

--- src/main/java/org/launchcode/hello_spring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hello_spring/controllers/HelloController.java
@@ -3,55 +3,47 @@ package org.launchcode.hello_spring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-//1.Add notation controller and import statement
 @Controller
 @ResponseBody
 @RequestMapping("hello")
 public class HelloController {
-    //2. add public string
-    //3. add response body
-    //4. add GetMapping
-    //change or handle requests at  path /hello
+    //handle request at path http://localhost:8080/hello
+
 //    @GetMapping("hello")
 //    @ResponseBody
-//    public String hello() {
+//    public String hello(){
 //        return "Hello, Spring!";
 //    }
 
+    //handle request at path //http://localhost:8080/hello/goodbye
     @GetMapping("goodbye")
-    //because putting @RequestMapping("hello") on top now this will live in /hello/goodbye
-    public String goodbye() {
-        return "goodbye, Spring!";
-
+    public String goodbye(){
+        return "Goodbye, Spring!";
     }
 
-    //Handles requests of the form /hello?name=Launchcode
-    //@RequestMapping is if I want to use both GET and POST
-    //because putting @RequestMapping("hello") on top now this will live in /hello/hello
+    //handle requests of the form http://localhost:8080/hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryRaram(@RequestParam String name) {
+    public String helloWithQueryParam(@RequestParam String name){
         return "Hello, " + name + "!";
     }
 
-    //Handles requests of the form /making?coder=a
-    //because putting @RequestMapping("hello") on top now this will live in /hello/making
-    @GetMapping("making")
-    public String testParam(@RequestParam String coder) {
-        return "what's up dog, " + coder + "!";
-    }
-
-    //Handle requests of the form /hello/LaunchCode
+    //handle requests of the form http://localhost:8080/hello/LaunchCode
     @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name) {
+    public String helloWithPathParam(@PathVariable String name){
         return "Hello, " + name + "!";
     }
 
-    //create form method
-    //hello/form
-    @GetMapping("form1")
+    //handles requests for http://localhost:8080/hello/form
+    @GetMapping("form")
     public String helloForm() {
-        return "form1";
+        return "<html>" +
+                "<body>" +
+                "<form action = '/hello' method = 'post'>" + // submit a request to /hello
+                "<input type = 'text' name = 'name' >" +
+                "<input type = 'submit' value = 'Greet Me!' >" +
+                "</form>" +
+                "</body>" +
+                "</html>";
     }
-//how to fix duplicating behavior put duplicate one up top
 
 }
